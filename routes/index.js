06594891940d8f18070503d2ca14750bf3a5e8f0
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2023-01-15 22:11:26 +0700

    add endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
+const student = require("./students");
 const auth = require("./auth");
+const report = require("./reports");
+const teacher = require("./teachers");
+const classes = require("./classes");
+const presence = require("./presences");
+const studies = require("./studies");
 
 router.get("/", (req, res) => {
   return res.status(200).json({
@@ -10,5 +16,11 @@ router.get("/", (req, res) => {
 });
 
 router.use("/auth", auth);
+router.use("/report", report);
+router.use("/student", student);
+router.use("/teacher", teacher);
+router.use("/class", classes);
+router.use("/presence", presence);
+router.use("/studies", studies);
 
 module.exports = router;
