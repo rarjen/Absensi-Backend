2224e14659136a09533d70f82cec2b43e9cb58f6
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2023-01-15 18:09:11 +0700

    create routes

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const authorize = require("../middlewares/authorize");
+const auth = require("../controllers/auth");
+
+router.post("/register", auth.register);
+router.post("/login", auth.login);
+router.get("/whoami", authorize(), auth.whoami);
