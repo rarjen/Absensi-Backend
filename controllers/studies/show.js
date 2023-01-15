057071c67bfc9fae8a946a3f67f63402e956c029
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2023-01-15 22:13:47 +0700

    add endpoint studies

---
+++ controllers/studies/show.js
@@ -0,0 +1,45 @@
+const { Studies, Classes } = require("../../models");
+
+const show = async (req, res, next) => {
+  try {
+    const studies = await Studies.findAll({
+      include: [
+        {
+          model: Classes,
+          as: "class",
+        },
+      ],
+    });
+
+    if (studies <= 0) {
+      return res.status(400).json({
+        jsonapi: {
+          version: "1.0",
+        },
+        meta: {
+          author: "Muhammad Umar Mansyur & Otniel Kevin Abiel",
+          copyright: "2022 ~ BE JavaScript Binar Academy",
+        },
+        status: 400,
+        message: "Data Class Kosong",
+      });
+    }
+
+    return res.status(200).json({
+      jsonapi: {
+        version: "1.0",
+      },
+      meta: {
+        author: "Muhammad Umar Mansyur & Otniel Kevin Abiel",
+        copyright: "2022 ~ BE JavaScript Binar Academy",
+      },
+      status: 200,
+      message: "Data berhasil ditampilkan",
+      data: studies,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = show;
