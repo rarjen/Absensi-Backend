b0b5ac0d6f5ada48f2053430f3b3979eef5f9e3e
Muhammad Umar Mansyur <umar@MacBook-Air-Muhammad.local>
2023-01-16 10:15:07 +0700

    Ke kampus dulu

--- controllers/official_reports/create_official_reports.js
+++ controllers/official_reports/create_official_reports.js
@@ -1,65 +1,63 @@
-const { Official_reports, Academic_years, precenses } = require("../../models");
+const { Official_reports, Academic_years, Learning_activities, Studies, Classes, Students } = require("../../models");
 
 const createReport = async (req, res, next) => {
   try {
     const { learning_activity_id, description, date, time } = req.body;
+    const response = await Official_reports.findAll(
+      {
+        include: [
+          {
+            model: Learning_activities,
+            as: "activity",
+            attributes: ["id", "study_id", "teacher_id", "status"],
+            include: [
+              {
+                model: Studies,
+                as: 'study',
+                include: [
+                  {
+                    model: Classes,
+                    as: 'class',
+                    include: [
+                      {
+                        model: Students,
+                        as: 'student'
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+        ],
+      }
+    );
+    console.log(response);
+    return res.json(response);
+    // const active = await Academic_years.findOne({ where: { status: true } });
 
-    const active = await Academic_years.findOne({ where: { status: true } });
-
-    const create = await Official_reports.create({
-      learning_activity_id,
-      academic_year_id: active.id,
-      description,
-      date,
-      time,
-    });
-
-    const response = await Official_reports.findOne({
-      where: { id: create.id },
-      include: [
-        {
-          model: Academic_years,
-          as: "academic_year",
-          attributes: ["id", "name", "status"],
-          where: { status: true },
-        },
-        {
-          model: learning_activity_id,
-          as: "learning_activity",
-          attributes: ["id", "name", "description", "date", "time"],
-          include: [
-            {
-              model: studies,
-              as: "study",
-              attributes: ["id", "name", "description"],
-            },
-            {
-
-            }
-          ],
-        },
-      ],
-    });
-
+    // const create = await Official_reports.create({
+    //   learning_activity_id,
+    //   academic_year_id: active.id,
+    //   description,
+    //   date,
+    //   time,
+    // });
     
 
-    const insertStudents = await precenses.create({
-      official_report_id: create.id,
-      
-    })
 
-    return res.status(201).json({
-      jsonapi: {
-        version: "1.0",
-      },
-      meta: {
-        author: "Muhammad Umar Mansyur & Otniel Kevin Abiel",
-        copyright: "2022 ~ BE JavaScript Binar Academy",
-      },
-      status: 201,
-      message: "Data berhasil ditambahkan",
-      data: create,
-    });
+    // return res.status(201).json({
+    //   jsonapi: {
+    //     version: "1.0",
+    //   },
+    //   meta: {
+    //     author: "Muhammad Umar Mansyur & Otniel Kevin Abiel",
+    //     copyright: "2022 ~ BE JavaScript Binar Academy",
+    //   },
+    //   status: 201,
+    //   message: "Data berhasil ditambahkan",
+    //   data: create,
+    // });
   } catch (error) {
     next(error);
   }
